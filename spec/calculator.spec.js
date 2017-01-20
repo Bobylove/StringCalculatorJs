c1bb658fa1a45cb73ae8c88b6e09986a2198b0d1
Fred <delon.frederic.pro.job@gmail.com>
2017-01-20 11:17:43 +0100

    Test 1  string " " = 0 clean

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -5,5 +5,8 @@ describe("StringCalculator execution", function () {
     it("should return 0 when input is null", function () {
         expect(StringCalculator(null)).toBe(0);
     });
+   it("string  =  0 ",function (){
+   	expect(StringCalculator("")).toBe(0);
+     });
 
 });
